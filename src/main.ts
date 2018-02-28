b1a45cb5387e250d4cbf4dea6f93d900fb8b0db7
petersaints <petersaints@gmail.com>
2018-02-28 17:04:28 +0000

    - Updated Dependencies (package.json)

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import DeepstreamCoordinator from "./DeepstreamCoordinator";
 import App from "./App";
 import User from "./User";
 
+var coordinator = new DeepstreamCoordinator("ws://localhost:6020/deepstream", new App("demo"), new User("jonhdoe", { password: "password123456" }));
+
 export {
     Coordinator,
     AbstractCoordinator,
